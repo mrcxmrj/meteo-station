aa97baa7bbc27066bf69533bbb34b6c670264e0d
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-06 22:41:25 +0200

    create setup script for dev env

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source .venv/bin/activate
+alias repl="sudo mpremote connect port:/dev/ttyACM0"
+
+if [[ -e config.py ]]; then
+    echo "Existing Wi-Fi config detected"
+else
+    echo "No Wi-Fi config detected, enter credentials"
+    echo -n "SSID: "
+    read wifi_ssid
+    echo -n "password: "
+    read -s wifi_password
+    echo; echo
+    touch config.py
+    echo "WIFI_SSID='$wifi_ssid'\nWIFI_PASSWORD='$wifi_password'" > config.py
+fi
+
+echo "Loading Wi-Fi config..."
+ repl cp config.py :
+echo "Done"
+echo "-------------------"
+echo "Available commands:"
+echo "repl - to open micropython interactive prompt (read-eval-print loop)"
+echo "run \$1 - to run your scripts"
+alias run="repl run"
