ee6c2f69d20e50804940e9e532b681e67a3a5209
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-06 22:56:27 +0200

    setup basic http server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import network
+import socket
 from config import WIFI_SSID, WIFI_PASSWORD
 
 ssid = WIFI_SSID
@@ -8,6 +9,16 @@ password = WIFI_PASSWORD
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
+
+html = """
+<!DOCTYPE html>
+<html>
+<head> <title>Pico W</title> </head>
+<body> <h1>Pico W</h1>
+<p>Hello World</p>
+</body>
+</html>
+"""
 
 # Wait for connect or fail
 max_wait = 10
@@ -25,3 +36,31 @@ else:
     print('connected')
     status = wlan.ifconfig()
     print( 'ip = ' + status[0] )
+
+# Open socket
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+
+# Listen for connections
+while True:
+    try:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        response = html
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(response)
+        cl.close()
+
+    except OSError as e:
+        cl.close()
+        print('connection closed')
