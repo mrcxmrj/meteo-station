f40fae7b92c74ea70623ca1e5f38a75000389330
mrcxmrj <marcin.zurawel@gmail.com>
2024-04-07 18:57:10 +0200

    format with isort and black

--- controllers/pico.py
+++ controllers/pico.py
@@ -1,9 +1,14 @@
 import time
+
 from machine import Pin
 
 led = Pin("LED", Pin.OUT)
 
-def blink_led ():
+
+def blink_led():
     led.value(1)
     time.sleep(0.5)
     led.value(0)
+
+
+# def read_temperature():
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import time
-import network
 import socket
-from config import WIFI_SSID, WIFI_PASSWORD
+import time
+
+import network
+
+from config import WIFI_PASSWORD, WIFI_SSID
 from controllers import pico
 
 ssid = WIFI_SSID
@@ -27,43 +29,44 @@ while max_wait > 0:
     if wlan.status() < 0 or wlan.status() >= 3:
         break
     max_wait -= 1
-    print('waiting for connection...')
+    print("waiting for connection...")
     time.sleep(1)
 
 # Handle connection error
 if wlan.status() != 3:
-    raise RuntimeError('network connection failed')
+    raise RuntimeError("network connection failed")
 else:
-    print('connected')
+    print("connected")
     status = wlan.ifconfig()
-    print( 'ip = ' + status[0] )
+    print("ip = " + status[0])
 
 # Open socket
-server_address = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+server_address = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
 
 server_socket = socket.socket()
 server_socket.bind(server_address)
 server_socket.listen(1)
 
-print('listening on', server_address)
+print("listening on", server_address)
 
 client_socket = None
 # Listen for connections
 while True:
     try:
         client_socket, remote_address = server_socket.accept()
-        print('client connected from', remote_address)
+        print("client connected from", remote_address)
         pico.blink_led()
-        cl_file = client_socket.makefile('rwb', 0)
+        cl_file = client_socket.makefile("rwb", 0)
         while True:
             line = cl_file.readline()
-            if not line or line == b'\r\n':
+            if not line or line == b"\r\n":
                 break
         response = html
-        client_socket.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        client_socket.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
         client_socket.send(response)
         client_socket.close()
 
     except OSError as e:
-        if client_socket: client_socket.close()
-        print('connection closed')
+        if client_socket:
+            client_socket.close()
+        print("connection closed")
