0d35f8390c75eee5695825c9eb10a4b558529395
mrcxmrj <marcin.zurawel@gmail.com>
2024-06-01 15:36:42 +0200

    add bmp280 to template

--- src/client/components/table_container.py
+++ src/client/components/table_container.py
@@ -8,19 +8,24 @@ class TableContainer:
         reader: Reader,
     ) -> None:
         self.reader = reader
-        self.temperature_table, self.humidity_table = self.create_data_tables()
+        self.temperature_table, self.humidity_table, self.pressure_table = (
+            self.create_data_tables()
+        )
 
-    def create_data_tables(self) -> tuple[Table, Table]:
-        temperature_headers = ["internal", "DHT11"]
+    def create_data_tables(self) -> tuple[Table, Table, Table]:
+        temperature_headers = ["internal", "DHT11", "BMP280"]
         humidity_headers = ["DHT11"]
+        pressure_headers = ["BMP280"]
 
         temperature_records = []
         humidity_records = []
+        pressure_records = []
 
         records = self.reader.read_saved_measurements(top=5)
         for record in records:
-            temperature_records.append(record[:2])
-            humidity_records.append(record[-1:])
+            temperature_records.append([record[0], record[2], record[4]])
+            humidity_records.append([record[1]])
+            pressure_records.append([record[3]])
 
         temperature_table = Table(
             temperature_headers,
@@ -32,34 +37,52 @@ class TableContainer:
             humidity_records,
             "%",
         )
-        return temperature_table, humidity_table
+        pressure_table = Table(
+            pressure_headers,
+            pressure_records,
+            "hPa",
+        )
+        return temperature_table, humidity_table, pressure_table
 
     def render(self) -> str:
-        current_board_temperature, current_temperature, current_humidity, *_ = (
-            self.reader.read_measurements()
-        )
+        (
+            current_board_temperature,
+            current_dht_humidity,
+            current_dht_temperature,
+            current_bmp_pressure,
+            current_bmp_temperature,
+        ) = self.reader.read_measurements()
         return f"""
             <div class="grid">
                 <article>
                     <h2>Temperature</h2>
                     <hr>
-                    <b>DHT11:</b> {current_temperature}°C
-                    <br>
                     <b>internal sensor:</b> {current_board_temperature}°C
+                    <br>
+                    <b>DHT11:</b> {current_dht_temperature}°C
+                    <br>
+                    <b>BMP280:</b> {current_bmp_temperature}°C
                     <hr>
                     <h3>History</h3>
-                    <hr>
                     {self.temperature_table.render()}
                 </article>
                 <article>
                     <h2>Humidity</h2>
                     <hr>
-                    <b>DHT11:</b> {current_humidity}%
-                    <br><br>
+                    <b>DHT11:</b> {current_dht_humidity}%
+                    <br><br><br>
                     <hr>
                     <h3>History</h3>
-                    <hr>
                     {self.humidity_table.render()}
+                </article>
+                <article>
+                    <h2>Pressure</h2>
+                    <hr>
+                    <b>BMP280:</b> {current_bmp_pressure}hPa
+                    <br><br><br>
+                    <hr>
+                    <h3>History</h3>
+                    {self.pressure_table.render()}
                 </article>
             </div>
         """
